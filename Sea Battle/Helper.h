5a6d777b197b0b48e1569de780472101e948d6a7
cam1k <149687485+cam1kk@users.noreply.github.com>
2024-05-27 17:30:41 +0400

    Update Helper.h

--- Sea Battle/Helper.h
+++ Sea Battle/Helper.h
@@ -22,6 +22,135 @@ int letternumber(char letter) {
 	else if (letter == 'I') return 9;
 	else if (letter == 'J') return 10;
 }
+-
+public interface IDatabaseAdapter
+{
+    void InsertUser(User user);
+    User FetchUser(int id);
+    bool DeleteUser(int id);
+    void ModifyUser(User user);
+}
+
+public class MsSqlDatabase
+{
+    public void InsertUser(User user) { }
+    public User FetchUser(int id) { return null; }
+    public bool DeleteUser(int id) { return true; }
+    public void Modify(User user) { }
+}
+
+public class MsSqlDatabaseAdapter : IDatabaseAdapter
+{
+    private readonly MsSqlDatabase _msSqlDatabase;
+
+    public MsSqlDatabaseAdapter(MsSqlDatabase msSqlDatabase)
+    {
+        _msSqlDatabase = msSqlDatabase;
+    }
+
+    public void InsertUser(User user)
+    {
+        _msSqlDatabase.InsertUser(user);
+    }
+
+    public User FetchUser(int id)
+    {
+        return _msSqlDatabase.FetchUser(id);
+    }
+
+    public bool DeleteUser(int id)
+    {
+        return _msSqlDatabase.DeleteUser(id);
+    }
+
+    public void ModifyUser(User user)
+    {
+        _msSqlDatabase.Modify(user);
+    }
+}
+
+public class MySqlDatabase
+{
+    public void InsertUser(User user) { }
+    public User GetUserById(int id) { return null; }
+    public bool RemoveUser(int id) { return true; }
+    public void Update(User user) { }
+}
+
+public class MySqlDatabaseAdapter : IDatabaseAdapter
+{
+    private readonly MySqlDatabase _mySqlDatabase;
+
+    public MySqlDatabaseAdapter(MySqlDatabase mySqlDatabase)
+    {
+        _mySqlDatabase = mySqlDatabase;
+    }
+
+    public void InsertUser(User user)
+    {
+        _mySqlDatabase.InsertUser(user);
+    }
+
+    public User FetchUser(int id)
+    {
+        return _mySqlDatabase.GetUserById(id);
+    }
+
+    public bool DeleteUser(int id)
+    {
+        return _mySqlDatabase.RemoveUser(id);
+    }
+
+    public void ModifyUser(User user)
+    {
+        _mySqlDatabase.Update(user);
+    }
+}
+
+public class PlSqlDatabase
+{
+    public void PushUser(User user) { }
+    public User GetUser(int id) { return null; }
+    public bool RemoveUser(int id) { return true; }
+    public void Modify(object user) { }
+}
+
+public class PlSqlDatabaseAdapter : IDatabaseAdapter
+{
+    private readonly PlSqlDatabase _plSqlDatabase;
+
+    public PlSqlDatabaseAdapter(PlSqlDatabase plSqlDatabase)
+    {
+        _plSqlDatabase = plSqlDatabase;
+    }
+
+    public void InsertUser(User user)
+    {
+        _plSqlDatabase.PushUser(user);
+    }
+
+    public User FetchUser(int id)
+    {
+        return _plSqlDatabase.GetUser(id);
+    }
+
+    public bool DeleteUser(int id)
+    {
+        return _plSqlDatabase.RemoveUser(id);
+    }
+
+    public void ModifyUser(User user)
+    {
+        _plSqlDatabase.Modify(user);
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}	
+-
 
 char numberletter(int number) {
 	if (number == 1) return 'A';
